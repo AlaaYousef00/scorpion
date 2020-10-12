469d33cfbcc068b944eb1e916cf4a066c7cfb05d
AlaaYousef00 <38228148+AlaaYousef00@users.noreply.github.com>
2020-10-12 06:46:08 +0200

    Add files via upload

---
+++ scorpion.py
@@ -0,0 +1,39 @@
+import requests, sys
+print("""
+   _____                           _               
+  / ____|                         (_)              
+ | (___    ___  ___   _ __  _ __   _   ___   _ __  
+  \___ \  / __|/ _ \ | '__|| '_ \ | | / _ \ | '_ \ 
+  ____) || (__| (_) || |   | |_) || || (_) || | | |
+ |_____/  \___|\___/ |_|   | .__/ |_| \___/ |_| |_|
+                           | |                     
+                           |_|                    
+""")
+
+fo = input("###### enter your list : ")
+fs = input("###### enter save list : ")
+http = input('##### Enter protocol : ')
+fc = open(fs, "w")
+f = open(fo, "r")
+frl = f.readlines()
+for link in frl:
+    if len(http) > 0:
+        if len(link.strip()) != 0:
+            link = http + "://" + link.strip()
+        else:
+            continue
+    else:
+        link = link.strip()
+    try:
+        r = requests.get(link, allow_redirects=True, timeout=5)
+        if r.status_code == 200:
+            print(r.url)
+            fc.write(r.url + "\n")
+        else:
+            print(link+ "========incorrect subdomain")
+    except (requests.ConnectionError, requests.TooManyRedirects, requests.exceptions.Timeout) as e:
+        pass
+
+f.close()
+fc.close()
+print("###################################\n ### finish process ### \n ###################################")
